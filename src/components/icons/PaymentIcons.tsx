65f73ed671b4c81e384320f0658ee60693a393d1
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-13 14:38:04 +0000

    https://i.postimg.cc/yd1XBk9H/5968382.png stripe , https://i.postimg.cc/

--- src/components/icons/PaymentIcons.tsx
+++ src/components/icons/PaymentIcons.tsx
@@ -6,6 +6,8 @@ const paymentMethods = [
   { name: 'Mastercard', src: 'https://i.postimg.cc/cJkZssmL/pngimg-com-mastercard-PNG15.png', width: 50, height: 32 },
   { name: 'American Express', src: 'https://i.postimg.cc/PryM21xq/amex.png', width: 50, height: 32 },
   { name: 'PayPal', src: 'https://i.postimg.cc/0Qkdxbm6/15465746.png', width: 50, height: 32 },
+  { name: 'Stripe', src: 'https://i.postimg.cc/yd1XBk9H/5968382.png', width: 50, height: 32 },
+    { name: 'Apple Pay', src: 'https://i.postimg.cc/63WLwyR5/apple-pay-og-twitter.jpg', width: 50, height: 32 },
 ];
 
 export function PaymentIcons() {
