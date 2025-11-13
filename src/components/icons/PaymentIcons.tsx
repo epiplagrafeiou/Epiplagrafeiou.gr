8fb5d2825bcdaf4e7b428f3e61242b0ac1decbd0
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-13 11:27:48 +0000

    what type of paymnets wil we be bale o accept ? iris , stripe , klarna ,

--- src/components/icons/PaymentIcons.tsx
+++ src/components/icons/PaymentIcons.tsx
@@ -6,11 +6,15 @@ const paymentMethods = [
   { name: 'Maestro', src: 'https://i.postimg.cc/Y0g0xCDJ/maestro.png', width: 50, height: 32 },
   { name: 'American Express', src: 'https://i.postimg.cc/PryM21xq/amex.png', width: 50, height: 32 },
   { name: 'PayPal', src: 'https://i.postimg.cc/RZB5sFhN/paypal.png', width: 50, height: 32 },
+  { name: 'Apple Pay', src: 'https://i.postimg.cc/bN3wVz3D/apple-pay.png', width: 50, height: 32 },
+  { name: 'Google Pay', src: 'https://i.postimg.cc/x15tXVr5/google-pay.png', width: 50, height: 32 },
+  { name: 'Klarna', src: 'https://i.postimg.cc/d11fS7jB/klarna.png', width: 50, height: 32 },
+  { name: 'IRIS', src: 'https://i.postimg.cc/L8p5ysV4/iris.png', width: 50, height: 32 },
 ];
 
 export function PaymentIcons() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex flex-wrap items-center justify-center gap-2">
       {paymentMethods.map((method) => (
         <div key={method.name} className="flex h-8 items-center justify-center rounded bg-white px-2 shadow-sm">
           <div className="relative" style={{ width: method.width, height: method.height }}>
