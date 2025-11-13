4759e47002dee1e5c77353d209cf6319ac156c8b
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-13 14:37:42 +0000

    actually emove teh imgaes i added i will give cropped for better site sp

--- src/components/icons/PaymentIcons.tsx
+++ src/components/icons/PaymentIcons.tsx
@@ -4,15 +4,8 @@ import Image from 'next/image';
 const paymentMethods = [
   { name: 'Visa', src: 'https://i.postimg.cc/L6Hj1fW9/visa.png', width: 50, height: 32 },
   { name: 'Mastercard', src: 'https://i.postimg.cc/cJkZssmL/pngimg-com-mastercard-PNG15.png', width: 50, height: 32 },
-  { name: 'Maestro', src: 'https://i.postimg.cc/Y0g0xCDJ/maestro.png', width: 50, height: 32 },
   { name: 'American Express', src: 'https://i.postimg.cc/PryM21xq/amex.png', width: 50, height: 32 },
-  { name: 'Stripe', src: 'https://i.postimg.cc/W4Yj227Q/5968382.png', width: 50, height: 32 },
-  { name: 'PayPal', src: 'https://i.postimg.cc/RZB5sFhN/paypal.png', width: 50, height: 32 },
-  { name: 'Apple Pay', src: 'https://i.postimg.cc/bN3wVz3D/apple-pay.png', width: 50, height: 32 },
-  { name: 'Google Pay', src: 'https://i.postimg.cc/JhYm11qw/Gpay-Icon-white-x2.png', width: 50, height: 32 },
-  { name: 'Klarna', src: 'https://i.postimg.cc/xdpY00RT/Marketing-Badge-With-Clear-Space.png', width: 50, height: 32 },
-  { name: 'IRIS', src: 'https://i.postimg.cc/L8p5ysV4/iris.png', width: 50, height: 32 },
-  { name: 'Εθνική Τράπεζα Ελλάδος', src: 'https://i.postimg.cc/q7D0kkLR/Logo-NBG-Rebranded-GR-crop.jpg', width: 50, height: 32 },
+  { name: 'PayPal', src: 'https://i.postimg.cc/0Qkdxbm6/15465746.png', width: 50, height: 32 },
 ];
 
 export function PaymentIcons() {
