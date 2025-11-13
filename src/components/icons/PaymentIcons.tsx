4d7609b3d46cb43fd29aca627d9c20369dcb804b
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-13 14:39:01 +0000

    https://i.postimg.cc/ZRbFZC3N/Logo-iris-hor-new.png iris ,https://i.post

--- src/components/icons/PaymentIcons.tsx
+++ src/components/icons/PaymentIcons.tsx
@@ -7,6 +7,8 @@ const paymentMethods = [
   { name: 'Apple Pay', src: 'https://i.postimg.cc/63WLwyR5/apple-pay-og-twitter.jpg', width: 50, height: 32 },
   { name: 'Mastercard', src: 'https://i.postimg.cc/V6fB1dMd/free-mastercard-logo-icon-svg-download-png-2944982.webp', width: 50, height: 32 },
   { name: 'Google Pay', src: 'https://i.postimg.cc/3R8C7dpw/google-pay-logo-1280x531.png', width: 50, height: 32 },
+  { name: 'IRIS', src: 'https://i.postimg.cc/ZRbFZC3N/Logo-iris-hor-new.png', width: 50, height: 32 },
+  { name: 'National Bank of Greece', src: 'https://i.postimg.cc/rmVCcKWw/Logo-NBG-Rebranded-GR-crop.jpg', width: 50, height: 32 },
 ];
 
 export function PaymentIcons() {
