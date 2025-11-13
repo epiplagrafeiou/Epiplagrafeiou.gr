e612f2bfd40508a909664d40b3175eb91366e29b
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-13 14:41:47 +0000

    for klrna , εθνκη ταπεζα and gogole pay replace wi th proveious images

--- src/components/icons/PaymentIcons.tsx
+++ src/components/icons/PaymentIcons.tsx
@@ -9,8 +9,8 @@ const paymentMethods = [
   { name: 'Apple Pay', src: 'https://i.postimg.cc/63WLwyR5/apple-pay-og-twitter.jpg', width: 50, height: 32 },
   { name: 'Google Pay', src: 'https://i.postimg.cc/3R8C7dpw/google-pay-logo-1280x531.png', width: 50, height: 32 },
   { name: 'IRIS', src: 'https://i.postimg.cc/ZRbFZC3N/Logo-iris-hor-new.png', width: 50, height: 32 },
-  { name: 'Klarna', src: 'https://i.postimg.cc/FRrgm7cN/Marketing-Badge-With-Clear-Space.png', width: 50, height: 32 },
-  { name: 'National Bank of Greece', src: 'https://i.postimg.cc/rmVCcKWw/Logo-NBG-Rebranded-GR-crop.jpg', width: 50, height: 32 },
+  { name: 'Klarna', src: 'https://i.postimg.cc/xdpY00RT/Marketing-Badge-With-Clear-Space.png', width: 50, height: 32 },
+  { name: 'National Bank of Greece', src: 'https://i.postimg.cc/q7D0kkLR/Logo-NBG-Rebranded-GR-crop.jpg', width: 50, height: 32 },
   { name: 'Piraeus Bank', src: 'https://i.postimg.cc/hjDbgfxQ/Piraeus-logo.jpg', width: 50, height: 32 },
 ];
 
