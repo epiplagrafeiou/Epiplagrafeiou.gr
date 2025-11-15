54b8bcfe4f3b759b48dcfb092b123492972c654f
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-15 22:47:58 +0000

    import { initializeApp, getApps, getApp } from "firebase/app"; import {

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,2 +1,12 @@
-// This file is being removed as part of the directory restructuring.
-// Its content has been moved to src/firebase/config.ts.
+// Firebase client SDK configuration
+// Used by the frontend (Next.js client components)
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBb3_QrL_dAYJKJcilx6WosaiQx0sSoPq0",
+    authDomain: "studio-2442325887-8d46d.firebaseapp.com",
+    projectId: "studio-2442325887-8d46d",
+    storageBucket: "studio-2442325887-8d46d.firebasestorage.app",
+    messagingSenderId: "853894628991",
+    appId: "1:853894628991:web:9f4c0d7443117059d6e14a"
+  };
+  
