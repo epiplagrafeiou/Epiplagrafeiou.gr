9002c416c65f1062c62f09e1a16428692149d68e
epiplagrafeiou dimitris <salesepiplagrafeiou@gmail.com>
2025-11-18 13:37:53 +0000

    Try fixing this error: `Runtime Error: useWishlist must be used within a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './providers';
 import { Inter } from 'next/font/google';
-import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
-import TopBar from '@/components/layout/TopBar';
-import { Toaster } from "@/components/ui/toaster";
-import NewsletterPopup from "@/components/layout/NewsletterPopup";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -24,18 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Providers>
-          <div className="flex min-h-screen flex-col">
-              <TopBar />
-              <Header />
-              <main className="flex-grow bg-white">
-                {children}
-              </main>
-              <Footer />
-              <Toaster />
-              <NewsletterPopup />
-          </div>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
